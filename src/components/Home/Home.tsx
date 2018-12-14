a410b9d958f9cd00a50bee2221be69b19cefb585
Aleksey Dorokhov <a.dorokhov@vsemayki.ru>
2018-12-14 14:57:52 +0700

    small fix

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,15 +1,46 @@
-import * as React from 'react'
-import * as classnames from 'classnames'
-import css from './Home.css'
+import * as React from 'react';
+import * as classnames from 'classnames';
+import css from './Home.css';
+import { Header, Table, Rating } from 'semantic-ui-react';
 
-export const Home: React.FunctionComponent = props =>
-  <div className={classnames('test', css.home)}>
-    <ul>
-      <li>
-        usage classnames in Home.tsx
-      </li>
-      <li>
-        Layout.tsx set background-color hot-pink using global styled jsx
-      </li>
-    </ul>
-  </div>
+export const Home: React.FunctionComponent = props => (
+    <Table celled padded>
+        <Table.Header>
+            <Table.Row>
+                <Table.HeaderCell colSpan="5">Git Repository</Table.HeaderCell>
+            </Table.Row>
+            <Table.Row>
+                <Table.HeaderCell singleLine>Evidence Rating</Table.HeaderCell>
+                <Table.HeaderCell>Effect</Table.HeaderCell>
+                <Table.HeaderCell>Efficacy</Table.HeaderCell>
+                <Table.HeaderCell>Consensus</Table.HeaderCell>
+                <Table.HeaderCell>Comments</Table.HeaderCell>
+            </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+            {[...Array(10)].fill(10).map((item, id) => (
+                <Table.Row>
+                    <Table.Cell>
+                        <Header as="h2" textAlign="center">
+                            A
+                        </Header>
+                    </Table.Cell>
+                    <Table.Cell singleLine>Weight</Table.Cell>
+                    <Table.Cell>
+                        <Rating icon="star" defaultRating={3} maxRating={3} />
+                    </Table.Cell>
+                    <Table.Cell textAlign="right">
+                        100% <br />
+                        <a href="#">65 studies</a>
+                    </Table.Cell>
+                    <Table.Cell>
+                        Creatine is the reference compound for power
+                        improvement, with numbers from one meta-analysis to
+                        assess potency
+                    </Table.Cell>
+                </Table.Row>
+            ))}
+        </Table.Body>
+    </Table>
+);
